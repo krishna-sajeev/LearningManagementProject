3125fa3eb34f4669ac0bc173368b71d40e42eccc
Nisha MV <nishakrishna1995@gmail.com>
2025-08-18 21:13:14 +0530

    final

--- backend/src/main/java/com/learningmanagement/backend/controller/FeedbackController.java
+++ backend/src/main/java/com/learningmanagement/backend/controller/FeedbackController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5175")
+@CrossOrigin(origins = "http://localhost:5173")
 public class FeedbackController {
 
     @Autowired
