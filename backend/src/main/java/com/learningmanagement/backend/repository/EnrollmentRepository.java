1d4a37c309745c23111df0e9ae5e75fbeee3aac7
krishna-sajeev <krishnapriyasajeev5@gmail.com>
2025-08-18 21:30:44 +0530

    enroll in progress]

--- backend/src/main/java/com/learningmanagement/backend/repository/EnrollmentRepository.java
+++ backend/src/main/java/com/learningmanagement/backend/repository/EnrollmentRepository.java
@@ -4,4 +4,6 @@ import com.learningmanagement.backend.model.Enroll;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enroll,Integer> {
+
+    Enroll findByCourse(String courseId);
 }
